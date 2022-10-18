65e9937a02127290abdb50cfc7a20f6e755a8f0a
redbor <redbor24@gmail.com>
2022-10-18 10:26:04 +0300

    Init commit

---
+++ tg.py
@@ -0,0 +1,51 @@
+import logging
+
+from environs import Env
+from telegram import ForceReply, Update
+from telegram.ext import (CallbackContext, CommandHandler, MessageHandler,
+                          Updater)
+from telegram.ext.filters import Filters
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(fr'Hi {user.mention_markdown_v2()}\!')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    env = Env()
+    env.read_env()
+    tg_token = env('TG_TOKEN')
+    updater = Updater(tg_token)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
