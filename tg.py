4956d9fd5d2b4299fd6ff1e4b2cac2a997247b72
redbor <redbor24@gmail.com>
2022-10-18 10:29:53 +0300

    Эхо-бот

--- tg.py
+++ tg.py
@@ -1,7 +1,7 @@
 import logging
 
 from environs import Env
-from telegram import ForceReply, Update
+from telegram import Update
 from telegram.ext import (CallbackContext, CommandHandler, MessageHandler,
                           Updater)
 from telegram.ext.filters import Filters
@@ -19,11 +19,6 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_markdown_v2(fr'Hi {user.mention_markdown_v2()}\!')
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     update.message.reply_text(update.message.text)
@@ -36,14 +31,10 @@ def main() -> None:
     updater = Updater(tg_token)
 
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     updater.start_polling()
-
     updater.idle()
 
 
